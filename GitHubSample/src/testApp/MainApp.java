7cee7df7e54bc207ae5fd8a2273670492ef17428
syk11 <syk11@DESKTOP-TTG1VI3>
2021-02-01 16:26:53 +0800

    ~3rd Version

--- GitHubSample/src/testApp/MainApp.java
+++ GitHubSample/src/testApp/MainApp.java
@@ -6,7 +6,7 @@ public class MainApp {
 		// TODO Auto-generated method stub
 		System.out.print("2nd Version");
 		//user 2 modify
-		System.out.print("3rd Version");
+		System.out.print("~3rd Version");
 	}
 
 }
